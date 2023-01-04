1355cce8b0347af5c3c11690be89de2ad758e88d
anderson <andersonbarreto2001@gmail.com>
2023-01-04 11:32:07 +0100

    part2 done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import { useState } from 'react'
 import UserCard from './components/UserCard'
 import { dataUsers } from './resources/dataSamples/dataUser'
 
-const ListUsers = () => {
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [userCard, setUserCard] = useState([])
+
+  const handleNextButton = () => {
+    if (currentIndex >= dataUsers().length - 1) {
+      setCurrentIndex(0)
+      setUserCard([])
+    } else {
+      setCurrentIndex(currentIndex + 1)
+      setUserCard([...userCard, dataUsers().map((element, index) => (
+        currentIndex === index
+          ? <UserCard
+              key={index}
+              userName={element.name.first + ' ' + element.name.last}
+              emailUser={element.email}
+              userImage={element.picture.medium}
+            />
+          : null
+      ))])
+
+      console.log(dataUsers()[currentIndex])
+    }
+    console.log(currentIndex)
+  }
+
   return (
     <div>
-      {dataUsers().map((element, index) => (
-        <UserCard
-          key={index}
-          userName={element.name.first + ' ' + element.name.last}
-          emailUser={element.email}
-          userImage={element.picture.medium}
-        />
-      ))}
+      <button onClick={handleNextButton}>Next</button>
+      {userCard}
     </div>
   )
-}
-
-const App = () => {
-  const [userCard, setUserCard] = useState([])
 }
 
 export default App
